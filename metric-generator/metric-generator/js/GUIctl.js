5c7acfbdd05e232a677e9e6224873809f32d59b9
castaway2000 <szablya@gmail.com>
2015-01-02 21:34:36 -0800

    Added all RMG files to project

---
+++ metric-generator/metric-generator/js/GUIctl.js
@@ -0,0 +1,33 @@
+﻿/*
+        Metrix RMG: (R)apid (M)etrics (G)enerator Version 1.0
+        Written and Coded by Adam Szablya
+        Project Started on:11/15/2014
+
+        Licencing Information:
+        Metrix RMG Version 1.0 is freely useable and re-distributable
+        under the GNU General Public Licence version 3.
+*/
+$(document).ready(function () {
+    $('#grid').draggable();
+
+// Load the Visualization API
+google.load('visualization', '1.0', { 'packages': ['corechart'] });
+google.setOnLoadCallback(drawChart);
+//variables start below here
+var sqlQuery = document.getElementById("sqlbox").value;
+//Code starts below here
+
+function drawChart() {
+    // Create the data table.
+
+
+    // Set chart options
+    var options = {
+        'title': document.getElementById("title").value,
+        'width': document.getElementById("width").value,
+        'height': document.getElementById("height").value
+    };
+
+
+}
+});
---
+++ metric-generator/metric-generator/rmg.Master.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace metric_generator
+{
+    public partial class rmg : System.Web.UI.MasterPage
+    {
+        //public SqlConnection con = new SqlConnection(@"Data Source=BLAZE-TURBO;Initial Catalog=AdventureWorks;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //SqlDataAdapter ad = new SqlDataAdapter("select top 10 SA.OrderQty, SH.OrderDate "+
+            //    "from Sales.SalesOrderDetail as SA "+
+            //    "join Sales.SalesOrderHeader as SH "+
+            //    "on SA.SalesOrderID = SH.SalesOrderID order by SA.OrderQty desc;",con);
+            //DataSet ds = new DataSet();
+            //ad.Fill(ds);
+            //GridView1.DataSource = ds;
+
+        }
+    }
+}
