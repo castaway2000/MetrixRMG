28d34c61d7e6720475fb94417cf49e4506e3428b
castaway2000 <szablya@gmail.com>
2015-03-11 01:17:38 -0700

    made code beautiful.

--- metric-generator/metric-generator/js/GUIctl.js
+++ metric-generator/metric-generator/js/GUIctl.js
@@ -10,24 +10,17 @@ google.setOnLoadCallback(function () {
     var sqldatachart1;
     var newdata = new google.visualization.DataTable();
 
-    //TODO: make graph data editable ie top sales
     newdata.addColumn('string', 'date');
-    newdata.addColumn('number', xtitle );
+    newdata.addColumn('number', xtitle);
+
     if (boxarray !== null) {
-        //console.log(titleChk, widthChk, heightChk);
-        console.log($('#ContentPlaceHolder1_graphselector').val());
-        newdata.addRows(boxarray);
-        /*==========================================\
-          TODO: make options user defined variables
-          TODO: bind user defined vairable from forms
-        \==========================================*/
         var options = {
             'title': xtitle || "",
             'width': xwidth || 400,
             'height': xheight || 400
         };
-
-
+        //console.log($('#ContentPlaceHolder1_graphselector').val());
+        newdata.addRows(boxarray);
         $('#ContentPlaceHolder1_graphselector').change(function () {
             graphtype = $(this).val();
             if (graphtype == "ColumnChart") { sqldatachart1 = new google.visualization.ColumnChart(document.getElementById('grid')); }
@@ -35,12 +28,14 @@ google.setOnLoadCallback(function () {
             if (graphtype == "LineChart") { sqldatachart1 = new google.visualization.LineChart(document.getElementById('grid')); }
             if (graphtype == "AreaChart") { sqldatachart1 = new google.visualization.AreaChart(document.getElementById('grid')); }
             if (graphtype == "PieChart") { sqldatachart1 = new google.visualization.PieChart(document.getElementById('grid')); }
-            console.log($('#ContentPlaceHolder1_graphselector').val());
-            if(document.getElementById('imgtickbox').checked){
-            google.visualization.events.addListener(sqldatachart1, 'ready', function () {
-                var imgUri = sqldatachart1.getImageURI();
-                window.open(imgUri);
-            });}
+            //console.log($('#ContentPlaceHolder1_graphselector').val());
+
+            if (document.getElementById('imgtickbox').checked) {
+                google.visualization.events.addListener(sqldatachart1, 'ready', function () {
+                    var imgUri = sqldatachart1.getImageURI();
+                    window.open(imgUri);
+                });
+            }
             sqldatachart1.draw(newdata, options);
         });
     }
